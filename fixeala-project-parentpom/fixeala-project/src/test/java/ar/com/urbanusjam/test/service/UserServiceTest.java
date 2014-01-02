7da7dc4dd7232cd7cf5c7020a3bff70469cfdb52
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-01-02 21:32:05 +0000

    Jasper Reports User Tests

---
+++ fixeala-project-parentpom/fixeala-project/src/test/java/ar/com/urbanusjam/test/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package ar.com.urbanusjam.test.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:test-context.xml"}) 
+public class UserServiceTest {
+
+}
