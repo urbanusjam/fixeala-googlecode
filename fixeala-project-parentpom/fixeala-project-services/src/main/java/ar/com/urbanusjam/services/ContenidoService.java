27bbb766e5c696768faf9c73bc28da3ce0cfa06f
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-01-07 21:07:42 +0000

    Servicios transaccionales.

--- fixeala-project-parentpom/fixeala-project-services/src/main/java/ar/com/urbanusjam/services/ContenidoService.java
+++ fixeala-project-parentpom/fixeala-project-services/src/main/java/ar/com/urbanusjam/services/ContenidoService.java
@@ -8,32 +8,41 @@ import java.util.List;
 import ar.com.urbanusjam.entity.annotations.Contenido;
 import ar.com.urbanusjam.services.dto.ContenidoDTO;
 import ar.com.urbanusjam.services.dto.FileWrapperDTO;
+import ar.com.urbanusjam.services.exceptions.BusinessException;
 
 public interface ContenidoService {
 	   
+         
+	//Contenidos
     
-    public void grabarContenido(ContenidoDTO contenido);
+    public FileWrapperDTO subirContenido(ContenidoDTO contenido) throws BusinessException; 
+    
+    public List<ContenidoDTO> listarContenido(Long idIssue) throws BusinessException;
+       
+    public void borrarContenido(ContenidoDTO contenido) throws BusinessException;    
+    
+    
+    //Files
+        
+    public FileWrapperDTO uploadFile(InputStream inputStream, String extensionArchivo) throws BusinessException; 
+    
+    public File abrirContenidoFile(ContenidoDTO contenidoDTO) throws BusinessException;
+    
+    
+    
+    
+    public InputStream abrirContenidoRaw(ContenidoDTO contenidoDTO) throws BusinessException;
+    
+    public ContenidoDTO obtenerContenido(Long idContenido) throws BusinessException;
+        
+    public boolean deleteFile(String filePath) throws BusinessException; 
+    
+    public int deleteMultipleFiles(String[] filePath) throws BusinessException; 
+        
+    //public void grabarContenido(ContenidoDTO contenido);    
   
-    public ContenidoDTO obtenerContenido(Long idContenido) throws FileNotFoundException;
-    
-    public List<ContenidoDTO> listarContenidos(Long idIssue);
-    
-    public void borrarContenido(List<ContenidoDTO> contenidos);
-    
-    
-    public FileWrapperDTO upload(ContenidoDTO contenido) throws FileNotFoundException; 
-    
-    public FileWrapperDTO subirContenido(InputStream inputStream, String extensionArchivo) throws FileNotFoundException; 
-    
-    public File abrirContenidoFile(ContenidoDTO contenidoDTO) throws FileNotFoundException;
-    
-    public InputStream abrirContenidoRaw(ContenidoDTO contenidoDTO) throws FileNotFoundException;
-    
-    
-   
-    //quitar
     public Contenido convertirAContenido(ContenidoDTO contenidoDTO);
-    
-    public ContenidoDTO convertirAContenidoDTO(Contenido contenido);
+  
+   public ContenidoDTO convertirAContenidoDTO(Contenido contenido);
 
 }
