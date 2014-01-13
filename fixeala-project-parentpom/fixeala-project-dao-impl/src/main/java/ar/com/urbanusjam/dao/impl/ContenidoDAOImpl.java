0353004c87d8ee5795dc531455d7fb313228a8ac
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-01-13 22:05:34 +0000

    FileUpload.

--- fixeala-project-parentpom/fixeala-project-dao-impl/src/main/java/ar/com/urbanusjam/dao/impl/ContenidoDAOImpl.java
+++ fixeala-project-parentpom/fixeala-project-dao-impl/src/main/java/ar/com/urbanusjam/dao/impl/ContenidoDAOImpl.java
@@ -4,14 +4,18 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.com.urbanusjam.dao.ContenidoDAO;
 import ar.com.urbanusjam.dao.impl.utils.GenericDAOImpl;
 import ar.com.urbanusjam.entity.annotations.Contenido;
+import ar.com.urbanusjam.entity.annotations.Issue;
 
 @Transactional(propagation= Propagation.REQUIRED, readOnly=false)
 public class ContenidoDAOImpl extends GenericDAOImpl<Contenido, Serializable> implements ContenidoDAO  {
@@ -28,7 +32,17 @@ public class ContenidoDAOImpl extends GenericDAOImpl<Contenido, Serializable> im
 
 	@Override
 	public List<Contenido> findContenidosByIssue(Long idIssue) {
-		List<Contenido> contenidos = this.findWhere(" issue.id = ? ", new Object[]{idIssue});    	
+//		List<Contenido> contenidos = this.findWhere(" issue.id = ? ", new Object[]{idIssue});    	
+		
+		
+		List<Contenido> contenidos = getSessionFactory().getCurrentSession().createCriteria(Contenido.class)  	
+			
+				.createAlias("issue", "i")
+				.add(Restrictions.eq("i.id", idIssue))
+		        .addOrder(Order.asc("orden") )
+		        .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY)
+		        .list();
+		
     	return contenidos; 
 	}	
 	
