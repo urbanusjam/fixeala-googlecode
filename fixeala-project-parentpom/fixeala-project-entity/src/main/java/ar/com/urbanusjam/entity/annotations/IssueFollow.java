12b45edddbeaad4ed9af96c2758707b78e04d7c8
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-02-06 04:27:59 +0000

    Arreglo de Composite Key de IssueFollow.

--- fixeala-project-parentpom/fixeala-project-entity/src/main/java/ar/com/urbanusjam/entity/annotations/IssueFollow.java
+++ fixeala-project-parentpom/fixeala-project-entity/src/main/java/ar/com/urbanusjam/entity/annotations/IssueFollow.java
@@ -6,6 +6,10 @@ import java.util.GregorianCalendar;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +21,15 @@ public class IssueFollow implements Serializable {
 	@EmbeddedId
     private IssueFollowPK id;
 	
+	@ManyToOne
+	@JoinColumn(name="ID_ISSUE", insertable=false, updatable=false)
+	private Issue issue;
+
+	
+	@ManyToOne
+	@JoinColumn(name="ID_FOLLOWER", insertable=false, updatable=false)
+	private User follower;
+	
 	@Column(name = "FOLLOW_DATE")
 	private GregorianCalendar date;
 	
@@ -24,13 +37,52 @@ public class IssueFollow implements Serializable {
 	public IssueFollow(){}
 
 
+	
+
+
 	public IssueFollowPK getId() {
 		return id;
 	}
 
+
+
+
+
 	public void setId(IssueFollowPK id) {
 		this.id = id;
 	}
+
+
+
+
+
+	public Issue getIssue() {
+		return issue;
+	}
+
+
+
+
+	public void setIssue(Issue issue) {
+		this.issue = issue;
+	}
+
+
+
+
+	public User getFollower() {
+		return follower;
+	}
+
+
+
+
+	public void setFollower(User follower) {
+		this.follower = follower;
+	}
+
+
+
 
 	public GregorianCalendar getDate() {
 		return date;
