862133a970b84d6964d10968166753850f84e0bb
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-02-27 20:55:15 +0000

    Corrección del script de MySQL:
    	- renombramiento de campos
    	- adición de campos
    	- letra minúscula
    	- constraints separadas

---
+++ trunk/fixeala-project-parentpom/fixeala-project/src/main/resources/scripts/db/fixeala.database.tables.sql
@@ -0,0 +1,374 @@
+/* ###################################################################################### */
+/* ###    FIXEALA Database Schema                                                         */ 	
+/* ###																					  */
+/* ###    Script by CORA REYES CALENS                                                     */ 	 				     
+/* ###    Done for UrbanusJam!		             										  */						 
+/* ###    Contact:     			                                                          */
+/* ###					urbanusjam@gmail.com                                              */				 
+/* ###   				coripel@gmail.com				                                  */	 
+/* ###																					  */
+/* ###    Created:  08-10-2013         													  */
+/* ###    © Copyright FIXEALA 2013-2014	             									  */
+/* ###################################################################################### */		
+
+
+DROP DATABASE IF EXISTS fixeala;
+CREATE DATABASE fixeala;
+
+
+USE fixeala;
+
+
+/* ###################################################################################### */
+/* ###    TABLES                                                                          */
+/* ###################################################################################### */
+
+
+-- ACTIVATION
+CREATE TABLE activation ( 
+	   token VARCHAR(126) NOT NULL,
+	   username VARCHAR(50) NOT NULL,		  
+	   creation_date DATETIME NOT NULL,
+	   expiration_date DATETIME NOT NULL,
+						
+	   PRIMARY KEY (token),
+	   UNIQUE KEY (username)
+	  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- AREA
+CREATE TABLE area ( 
+	   id_area BIGINT(20) NOT NULL AUTO_INCREMENT,
+	   area_name VARCHAR(255) NULL,
+	   acronym VARCHAR(15) NULL,
+	   city VARCHAR(255) NULL,
+	   city_acronym VARCHAR(2) NULL,
+	   province VARCHAR(15) NULL,
+	   province_acronym VARCHAR(2) NULL,
+	   
+	   PRIMARY KEY (id_area)
+	   UNIQUE KEY (areaname)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- COMMENT			
+CREATE TABLE comment ( 	  
+	   id_comment BIGINT(20) NOT NULL AUTO_INCREMENT,    
+	   id_issue BIGINT(20) NOT NULL, 
+	   id_user BIGINT(20) NOT NULL,	   
+	   creation_date DATETIME NULL,    	   
+	   message VARCHAR(300) NOT NULL,		 
+	   flag TINYINT(1) NOT NULL,
+	   
+	   PRIMARY KEY(id_comment)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- CONTENIDO
+CREATE TABLE content ( 
+	   id_content BIGINT(20) NOT NULL AUTO_INCREMENT,
+	   id_issue BIGINT(20) NULL,	   
+	   height BIGINT(22) NOT NULL,
+	   width BIGINT(22) NOT NULL,
+	   filename VARCHAR(255) NOT NULL,
+	   filename_extension VARCHAR(255) NOT NULL,	
+	   filename_with_extension VARCHAR(255) NOT NULL,
+	   relative_path VARCHAR(255) NOT NULL,		  
+	   upload_date DATETIME NULL,
+	   file_order INT(10) NOT NULL,
+	   is_profile_pic TINYINT(1) NOT NULL,
+	   
+	   PRIMARY KEY (id_content)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ISSUE
+CREATE TABLE issue (
+	   id_issue BIGINT(20) NOT NULL, 
+	   id_reporter BIGINT(20) NOT NULL,
+	   id_assigned_official BIGINT(20) NULL,
+	   id_area BIGINT(20) NULL,
+	   creation_date DATETIME NOT NULL,    	  
+	   last_update_date DATETIME NOT NULL,    	  
+	   address VARCHAR(255) NOT NULL,
+	   neighborhood VARCHAR (64) NULL,  
+	   city VARCHAR(255) NOT NULL,		
+	   province VARCHAR (64) NOT NULL,  		   
+	   latitude FLOAT NOT NULL,
+	   longitude FLOAT NOT NULL,
+	   title VARCHAR(255) NOT NULL,
+	   description LONGTEXT NOT NULL,		  
+	   status VARCHAR(30) NOT NULL, 
+	   priority VARCHAR(30) NOT NULL, 
+	   
+	   PRIMARY KEY (id_issue)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ISSUE_FOLLOW
+CREATE TABLE issue_follow (	 	
+	   id_issue BIGINT(20) NOT NULL,
+	   id_follower BIGINT(20) NOT NULL,	 
+	   follow_date DATETIME NOT NULL,
+	   	  
+	   PRIMARY KEY (id_issue, id_follower)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ISSUE LICITACION	
+CREATE TABLE issue_licitacion ( 	  
+	   id_issue BIGINT(20) NOT NULL, 	
+	   nro_licitacion VARCHAR(20) NULL, 
+	   nro_expediente VARCHAR(20) NULL, 	
+	   objeto VARCHAR (600) NULL, 
+	   tipo VARCHAR(20) NULL,  	
+	   valor_pliego FLOAT(20) NULL,
+	   documentacion_pliego VARCHAR(255) NULL,	
+	   unidad_ejecutora VARCHAR (255) NULL, 
+	   unidad_financiamiento VARCHAR (255) NULL, 
+	   empresa_constructora_nombre VARCHAR (255) NULL, 
+	   empresa_constructora_cuit BIGINT (30) NULL, 
+	   empresa_constructora_email VARCHAR (255) NULL, 
+	   representante_tecnico_nombre VARCHAR (255) NULL, 
+	   representante_tecnico_dni BIGINT (30) NULL, 
+	   representante_tecnico_email VARCHAR (255) NULL, 
+	   plazo_ejecucion INT(10) NULL, 
+	   presupuesto_adjudicado FLOAT (20) NULL,
+	   presupuesto_final FLOAT (20) NULL,
+	   fecha_estimada_inicio DATETIME NULL,
+	   fecha_estimada_fin DATETIME NULL,
+	   fecha_real_inicio DATETIME NULL,
+	   fecha_real_fin DATETIME NULL,
+	   stauts_obra VARCHAR(30) NULL,  
+	   
+	   KEY(id_issue)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;	
+
+
+-- ISSUE_TAG
+CREATE TABLE issue_tag (
+	   id_issue BIGINT(20) NOT NULL,
+	   id_tag BIGINT(20) NOT NULL,		   
+	   
+	   PRIMARY KEY (id_tag, id_issue)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ISSUE_UPDATE_HISTORY
+CREATE TABLE issue_update_history ( 	  
+	   id_issue_history BIGINT(20) NOT NULL AUTO_INCREMENT, 	   
+	   id_issue BIGINT(20) NOT NULL, 	   
+	   id_user BIGINT(20) NOT NULL,	   
+	   update_date DATETIME NOT NULL,    	   
+	   status VARCHAR(30) NOT NULL, 	    
+	   operation_type VARCHAR(1) NOT NULL,
+	   motive VARCHAR(255) NOT NULL, 	
+	   observations VARCHAR(500) NULL,  
+	   modified_fields VARCHAR(255) NULL, 
+	   
+	   PRIMARY KEY(id_issue_history),
+	   KEY(id_issue)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ISSUE_VIEW
+CREATE TABLE issue_pageview (	
+	   id_issue_pageview BIGINT(20) NOT NULL AUTO_INCREMENT,    
+	   id_issue BIGINT(20) NOT NULL,
+	   id_user BIGINT(20) NOT NULL,	 
+	   ip_address INT(11) UNSIGNED NULL,
+	   pageview_date DATETIME NOT NULL,
+	   
+	   PRIMARY KEY (id_issue_pageview)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ISSUE_VOTE
+CREATE TABLE issue_vote (	
+       id_issue BIGINT(20) NOT NULL,
+	   id_voter BIGINT(20) NOT NULL,	 
+	   vote SMALLINT NOT NULL CHECK ("VOTE" = 1 OR "VOTE" = -1),
+	   vote_date DATETIME NOT NULL,
+	   	   
+	   PRIMARY KEY (id_issue, id_voter) 
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- PASSWORD CHANGE REQUEST 
+CREATE TABLE password_change_request ( 
+	   token VARCHAR(126) NOT NULL,
+	   username VARCHAR(50) NOT NULL,		  
+	   creation_date DATETIME NOT NULL,
+	   expiration_date DATETIME NOT NULL,
+				
+	   PRIMARY KEY (token)
+	  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- PERSISTENT LOGIN (REMEMBER ME)
+CREATE TABLE persistent_logins(
+	   username VARCHAR(50) NULL,
+	   series VARCHAR(64) NULL,
+	   token VARCHAR(64) NULL,
+	   last_used TIMESTAMP NULL,		      
+	   
+	   PRIMARY KEY (series)
+	 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ROLE
+CREATE TABLE role (
+	   id_role BIGINT(20) NOT NULL AUTO_INCREMENT,
+	   rolename VARCHAR(50) NOT NULL,
+		
+	   PRIMARY KEY (id_role)	
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- TAG
+CREATE TABLE tag (
+	   id_tag BIGINT(20) NOT NULL AUTO_INCREMENT,
+	   tagname VARCHAR (50) NOT NULL,
+	   
+	   PRIMARY KEY (id_tag)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;	
+
+
+-- USER 	
+CREATE TABLE user ( 
+	   id_user BIGINT(20) NOT NULL AUTO_INCREMENT,
+	   username VARCHAR(50) NOT NULL,
+	   password VARCHAR(50) NULL,    
+	   email VARCHAR(255) NOT NULL,
+	   name VARCHAR(255) NULL,
+	   last_name VARCHAR(255) NULL,
+	   place_of_residence VARCHAR(64) NULL,  /* <neighborhood>, <city>, <province> */
+	   salt VARCHAR(25) NULL,	
+	   id_area BIGINT(20) NULL,
+	   gov_position VARCHAR(255) NULL,
+	   gov_sub_area VARCHAR(255) NULL,
+	   gov_sub_area_acronym VARCHAR(15) NULL,
+	   is_area TINYINT(1) NOT NULL,
+	   is_verified_official TINYINT(1) NOT NULL,
+	   registration_date DATETIME NULL,     
+	   last_password_change_date DATETIME NULL,    	   
+	   last_login_date DATETIME NULL,   
+	   closed_account_date DATETIME NULL,   	   
+	   enabled TINYINT(1) NOT NULL,		
+	   
+	   PRIMARY KEY (id_user),
+	   UNIQUE KEY (email, username)	 
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- USER_ROLE
+CREATE TABLE user_role (
+	   id_user BIGINT(20) NOT NULL,
+	   id_role VARCHAR(50) NOT NULL,		
+	   
+	   PRIMARY KEY (id_user, id_role)
+	   
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+/* ###################################################################################### */
+/* ###    CONSTRAINTS                                                                     */
+/* ###################################################################################### */
+
+
+ALTER TABLE user
+    ADD CONSTRAINT FOREIGN KEY fk_user_1 (id_area)
+    REFERENCES area (id_area);
+        
+ALTER TABLE user_role
+    ADD CONSTRAINT FOREIGN KEY fk_user_role_1 (id_user)
+    REFERENCES user (id_user);
+    
+ALTER TABLE issue
+    ADD CONSTRAINT FOREIGN KEY fk_issue_1 (id_reporter)
+    REFERENCES user (id_user);
+    
+ALTER TABLE issue
+    ADD CONSTRAINT FOREIGN KEY fk_issue_2 (id_assigned_official)
+    REFERENCES user (id_user);
+    
+ALTER TABLE issue
+    ADD CONSTRAINT FOREIGN KEY fk_issue_3 (id_area)
+    REFERENCES area (id_area);
+
+ALTER TABLE issue_licitacion
+    ADD CONSTRAINT FOREIGN KEY fk_issue_licitacion_1 (id_issue)
+    REFERENCES issue (id_issue)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+    
+ALTER TABLE issue_tag
+    ADD CONSTRAINT FOREIGN KEY fk_issue_tag_1 (id_tag)
+    REFERENCES tag (id_tag);
+    
+ALTER TABLE issue_tag
+    ADD CONSTRAINT FOREIGN KEY fk_issue_tag_2 (id_issue)
+    REFERENCES issue (id_issue);
+    
+ALTER TABLE content
+    ADD CONSTRAINT FOREIGN KEY fk_content_1 (id_issue)
+    REFERENCES issue (id_issue);
+    
+ALTER TABLE issue_update_history
+    ADD CONSTRAINT FOREIGN KEY fk_issue_update_history_1 (id_issue)
+    REFERENCES issue (id_issue)
+    ON DELETE CASCADE ON UPDATE CASCADE;
+    
+ALTER TABLE issue_update_history
+    ADD CONSTRAINT FOREIGN KEY fk_issue_update_history_2 (id_user)
+    REFERENCES user (id_user);
+
+ALTER TABLE comment
+    ADD CONSTRAINT FOREIGN KEY fk_comment_1 (id_issue)
+    REFERENCES issue (id_issue);
+    
+ALTER TABLE comment
+    ADD CONSTRAINT FOREIGN KEY fk_comment_2 (id_user)
+    REFERENCES user (id_user);
+    
+ALTER TABLE issue_vote
+    ADD CONSTRAINT FOREIGN KEY fk_issue_vote_1 (id_issue)
+    REFERENCES issue (id_issue);
+    
+ALTER TABLE issue_vote
+    ADD CONSTRAINT FOREIGN KEY fk_issue_vote_2 (id_voter)
+    REFERENCES user (id_user);
+
+ALTER TABLE issue_follow
+    ADD CONSTRAINT FOREIGN KEY fk_issue_follow_1 (id_issue)
+    REFERENCES issue (id_issue);
+    
+ALTER TABLE issue_follow
+    ADD CONSTRAINT FOREIGN KEY fk_issue_follow_2 (id_follower)
+    REFERENCES user (id_user);
+    
+ALTER TABLE issue_pageview
+    ADD CONSTRAINT FOREIGN KEY fk_issue_pageview_1 (id_issue)
+    REFERENCES issue (id_issue);
+    
+ALTER TABLE issue_pageview
+    ADD CONSTRAINT FOREIGN KEY fk_issue_pageview_2 (id_user)
+    REFERENCES user (id_user);
+
