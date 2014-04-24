e8e80272ec6a0feb26552258b53830b535a95d43
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-04-24 04:38:06 +0000

    Widget v1: se agrega url para redirigir los links a la página de cada reclamo.

--- fixeala-project-parentpom/fixeala-project/src/main/webapp/widget/js/widget.js
+++ fixeala-project-parentpom/fixeala-project/src/main/webapp/widget/js/widget.js
@@ -1,46 +1,52 @@
-$(document).ready(function(){
-	
-	var loader = '<div class="widgetLoader"><img src="http://localhost:8080/fixeala/widget/images/loader.gif"/></span>';	
-	var $widgetBody =  $('.widgetIssues');
-	
-	$('#refreshWidget').click(function() {	
-		$.ajax({
-		    url: "./refreshWidget.html",
-	 		type: "GET",		 		
-	 		dataType: "json",									 
-	        success: function(data){	        	
-	        	        	
-	          	$widgetBody.replaceWith(loader);
-	        	
-	        	if(data.result){
-	        		$widgetBody.load(location.href + " .widgetIssues > * ");	
-	 				setTimeout(function(){	 				
-	 					$('.widgetLoader').replaceWith($widgetBody);					 					 						      
-	 				}, 1000);
-	        	}	        	
-	        	else{
-	        		setTimeout(function(){		
-	        			$('.widgetLoader').hide();
-	        			$(".widgetError").text(data.message);
-	 					$(".widgetError").show();	 					
-	 				}, 1000);
-	        	}
-	        	
-    		}, // success 
-    		
-    		error: function(jqXHR, exception) {
-    			
-    			$widgetBody.replaceWith(loader);
-    			
-    			setTimeout(function(){	 	
- 					$(".widgetError").text("Ha ocurrido un error al intentar cargar los reclamos. Intente de nuevo.");
- 					$('.widgetLoader').hide();	
-					$(".widgetError").show();	
- 				}, 1000);
-    			
-            } // error
-		});			  
-	});	
-	
-});
+var loader = '<div class="widgetLoader"><img src="http://localhost:8080/fixeala/widget/images/loader.gif"/></span>';	
+var $widgetBody =  $('.widgetIssues');
 
+function redirectIssueURL(issueID, issueTitle){
+	var protocol = window.location.protocol;
+	var host = window.location.host;
+	var context = "fixeala";
+	var subcontext = "issues/" + issueID;		
+	var parsedTitle = issueTitle.replace(/\s/g, '-').toLowerCase();	
+	var url = protocol + "//" + host + "/" + context + "/" + subcontext + "-" + parsedTitle + ".html";
+	window.open (url, issueTitle);
+}
+	
+	
+$('#refreshWidget').click(function() {	
+	$.ajax({
+	    url: "./refreshWidget.html",
+ 		type: "GET",		 		
+ 		dataType: "json",									 
+        success: function(data){	        	
+        	        	
+          	$widgetBody.replaceWith(loader);
+        	
+        	if(data.result){
+        		$widgetBody.load(location.href + " .widgetIssues > * ");	
+ 				setTimeout(function(){	 				
+ 					$('.widgetLoader').replaceWith($widgetBody);					 					 						      
+ 				}, 1000);
+        	}	        	
+        	else{
+        		setTimeout(function(){		
+        			$('.widgetLoader').hide();
+        			$(".widgetError").text(data.message);
+ 					$(".widgetError").show();	 					
+ 				}, 1000);
+        	}
+        	
+		}, // success 
+		
+		error: function(jqXHR, exception) {
+			
+			$widgetBody.replaceWith(loader);
+			
+			setTimeout(function(){	 	
+				$(".widgetError").text("Ha ocurrido un error al intentar cargar los reclamos. Intente de nuevo.");
+				$('.widgetLoader').hide();	
+				$(".widgetError").show();	
+			}, 1000);
+			
+        } // error
+	});			  
+});	
