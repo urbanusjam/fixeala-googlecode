dfa177047149822c6b3aa6b07e2a477508143f30
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-06-05 21:02:16 +0000

    Cambios en las configuraciones de Spring. Implementación de MailService y métodos transaccionales.

--- fixeala-project-parentpom/fixeala-project-services/src/main/java/ar/com/urbanusjam/services/MailService.java
+++ fixeala-project-parentpom/fixeala-project-services/src/main/java/ar/com/urbanusjam/services/MailService.java
@@ -1,5 +1,11 @@
 package ar.com.urbanusjam.services;
 
 public interface MailService {
+	
+	public void sendActivationRequestEmail(String username, String token, String email) throws Exception;
+	
+	public void sendActivationSuccessEmail(String username, String email) throws Exception;
+	
+	public void sendPasswordResetEmail(String username, String token, String email) throws Exception;	
 
 }
