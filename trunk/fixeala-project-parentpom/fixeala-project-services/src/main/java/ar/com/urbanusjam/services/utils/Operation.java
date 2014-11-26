ee4d815e355e98f46769a1c46181a1d9dbaec7b9
urbanusjam@gmail.com <urbanusjam@gmail.com@381b9414-bd5f-bda2-2513-cd79c626965f>
2014-11-26 22:48:40 +0000

    - Eliminación de algunos DTOs. - Paquete UTILS pasa a proyecto Entity.

--- trunk/fixeala-project-parentpom/fixeala-project-services/src/main/java/ar/com/urbanusjam/services/utils/Operation.java
+++
@@ -1,9 +0,0 @@
-package ar.com.urbanusjam.services.utils;
-
-public final class Operation {
-	
-	public static final String CREATE = "A";
-	public static final String DELETE = "B";
-	public static final String UPDATE = "M";	
-
-}
